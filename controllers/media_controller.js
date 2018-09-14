57ee4d913f528316745b3a86fa8f984933d58842
aehaq <azfarehaq@gmail.com>
2018-09-13 21:22:48 -0700

    ReadMe Complete

--- controllers/media_controller.js
+++ controllers/media_controller.js
@@ -13,14 +13,14 @@ router.post("/api/media", function(req, res) {
     console.log(req.body)
     media.insertOne("media_name", req.body.media_name, function(result) {
         res.json({ id: result.insertId})
-    })
+    });
 });
 
 router.put("/api/media/:id", function(req, res) {
     condition = "id = " + req.params.id
     media.updateOne({completed: 1}, condition, function(result) {
         res.json(result)
-    })
-})
+    });
+});
 
 module.exports = router;
